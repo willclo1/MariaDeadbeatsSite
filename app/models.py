a6a0c1df50ddb659476bb71003f84a26e898e478
nameofme01 <69026352+nameofme01@users.noreply.github.com>
2024-11-26 16:22:15 -0600

    created admin home for site and marked out some actions

--- app/models.py
+++ app/models.py
@@ -17,31 +17,20 @@ class Users(UserMixin, db.Model):
     time_of_last_access: so.Mapped[datetime] = so.mapped_column(
         index=True, default=lambda: datetime.now(timezone.utc))
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    is_admin: so.Mapped[bool] = so.mapped_column(sa.Boolean)
 
 
     def __repr__(self):
         return '<Users {}>'.format(self.username)
+
+    def set_is_admin(self, is_admin: bool):
+        self.is_admin = is_admin
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-
-
-class Admins(UserMixin, db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
-                                                unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
-                                             unique=True)
-    # can use this for a bonus feature - last user access
-    time_of_last_access: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.utc))
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
-    def __repr__(self):
-        return '<Admins {}>'.format(self.username)
 
 @login.user_loader
 def load_admin(user_id):
