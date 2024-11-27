8ed10183c48d8b416254509fc4b80a6cd70355d1
willclo1 <115596953+willclo1@users.noreply.github.com>
2024-11-27 12:46:34 -0600

    added a guest user

--- cfg.py
+++ cfg.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # want to unify all of this
 
 # Modify for your own database connection/account
-cfg = {"host" : "localhost", "user": "willclore", "password" : "password", "db" : "MariaDeadbeats"}
+cfg = {"host" : "localhost", "user": "guest_user", "password" : "guestPassword123", "db" : "MariaDeadbeats"}
 engineStr = "mysql+pymysql://" + cfg.get("user") + ":" + cfg.get("password") + "@" + cfg.get(
         "host") + ":3306/" + cfg.get("db")
 
