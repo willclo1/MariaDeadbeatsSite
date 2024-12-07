fdbefa947b6f06e6a651eccf5fecdb0b17762bbd
willclo1 <115596953+willclo1@users.noreply.github.com>
2024-12-07 13:01:29 -0600

    nevermind

---
+++ cfg.py
@@ -0,0 +1,18 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+# Modify for your own database connection/account
+cfg = {"host" : "localhost", "user": "guest_user", "password" : "guestPassword123", "db" : "MariaDeadbeats"}
+engineStr = "mysql+pymysql://" + cfg.get("user") + ":" + cfg.get("password") + "@" + cfg.get(
+        "host") + ":3306/" + cfg.get("db")
+
+
+
+sportradar_api_key = 'Ba2s9dFhXNxg4mSfShenScIRzO7fbXpReFyS6cGx'
+
+# configuration for database can config from class config to simplify configuration
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    DATABASE_URL = os.environ.get('DATABASE_URL') or "mysql+pymysql://guest_user:guestPassword123@localhost/MariaDeadbeats"
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
