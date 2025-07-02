0b6cf7f00e2dd8ade74fea850730c973028f64a3
Will Clore <115596953+willclo1@users.noreply.github.com>
2025-07-02 13:01:55 -0500

    Update MariaDeadbeatsSite.py

--- MariaDeadbeatsSite.py
+++ MariaDeadbeatsSite.py
@@ -1,2 +1,7 @@
-# do this so flask knows how to run the app
+# run.py
 from app import app
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
